c625e1573a6fca2fccf91c1aeb22c501b0988ef7
Patrick Yuanto <100013670+Lucid-H2O@users.noreply.github.com>
2023-02-06 12:51:08 +0800

    Update ps4_controller.py

--- turtle_ps4/src/ps4_controller.py
+++ turtle_ps4/src/ps4_controller.py
@@ -6,14 +6,15 @@ from turtlesim.srv import SetPen
 from std_srvs.srv import Empty
 # hint: some imports are missing
 
-
+multi_pos = 0
 old_data = Ps4Data()
 
 def callback(data):
     global old_data
+    global multi_pos
     
     speed_multi =[1,1.5,2,2.5,3]
-    int multi_pos = 0
+    
     
     vel = Twist()
     
@@ -33,24 +34,24 @@ def callback(data):
     
     #shape buttons change color
     if ((data.cross == true) and (old_data.cross == false)):
-    srv_col((0,0,255))
+    srv_col((0,0,255),1,1)
     
     if ((data.circle == true) and (old_data.circle == false)):
-    srv_col((255,0,0))
+    srv_col((255,0,0),1,1)
     
     if ((data.triangle == true) and (old_data.triangle == false)):
-    srv_col((0,255,0))
+    srv_col((0,255,0),1,1)
     
     if ((data.square == true) and (old_data.square == false)):
-    srv_col((255,0,255))
+    srv_col((255,0,255),1,1)
     
     
     #change multiplier levels
     if ((data.dpad_y == 1) and (old_data.dpad_y == 0)):
-    multi_pos += 1
+        multi_pos += 1
     	
     if ((data.dpad_x == 1) and (old_data.dpad_x == 0)):
-    multi_pos -= 1
+        multi_pos -= 1
     
     if multi_pos>4:
     	multi_pos=4
@@ -64,7 +65,7 @@ if __name__ == '__main__':
     rospy.init_node('ps4_controller')
     
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 1) # publisher object goes here... hint: the topic type is Twist
-    sub = rospy.Subscriber('/input/ps4_data', Ps4Data, queue_size=1) # subscriber object goes here
+    sub = rospy.Subscriber('/input/ps4_data', Ps4Data, callback) # subscriber object goes here
     
     # one service object is needed for each service called!
     srv_col = rospy.ServiceProxy('/turtle1/set_pen', SetPen)# service client object goes here... hint: the srv type is SetPen
